d21a6d9d6805875f1387d67d6aa43827d12ece2b
Mohammad Israk Chowdhury <israkchowdhury03@gmail.com>
2025-02-09 23:46:02 +0600

    firebase secured

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBzRwAQvzS1CaMX9dLak44EijrtnKjKuCA",
-  authDomain: "visa-nav.firebaseapp.com",
-  projectId: "visa-nav",
-  storageBucket: "visa-nav.firebasestorage.app",
-  messagingSenderId: "331016086538",
-  appId: "1:331016086538:web:61c3962fb9f9a35cd72bf3",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
